d054af5f9e1abbfd1fe931181fdb05a055691113
BenardBett <kiplangatbett5@gmail.com>
2020-11-14 01:34:23 +0300

    created methods in profile component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { RepoComponent } from './repo/repo.component';
+import { ProfileComponent } from './profile/profile.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -9,8 +9,30 @@ export class ProfileComponent implements OnInit {
 profile: any[];
 repos: any[];
 username: any[];
-  constructor() { }
-
+  constructor( private githubservice: GithubService) { 
+    this.githubservice.getdata().subscribe((profile:any[])=>
+    console.log(profile);
+    this.profile= profile;
+  }ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+);
+  this.githubservice.getProfileRepos().subscribe((repos:any[]=>{
+    console.log(repos);
+    this.repos= repos;
+  });
+}
+findProfile(){
+  this.githubservice.updateProfile(this.username);
+  this.githubservice.getdata().subscribed((profile:any[])=>{
+    console.log(profile);
+    this.profile=profile;
+  });
+  this.githubservice.getProfileRepos().subscribe((repos: any[]) => {
+    console.log(repos);
+    this.repos = repos;
+  });
+}
   ngOnInit(): void {
   }
 
