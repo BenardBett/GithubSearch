6ef124d119f5b6021e46ffefd5b4f89715609cc6
BenardBett <kiplangatbett5@gmail.com>
2020-11-14 00:35:39 +0300

    created a repo html structure and its logic and importing the services

--- src/app/repo/repo.component.ts
+++ src/app/repo/repo.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GithubService } from '../github.service';
 @Component({
   selector: 'app-repo',
   templateUrl: './repo.component.html',
   styleUrls: ['./repo.component.css']
 })
 export class RepoComponent implements OnInit {
+  repos:any;
+  profile: any;
 
-  constructor() { }
+  constructor( private githubservice: GithubService) {
+      this.githubservice = githubservice;
 
-  ngOnInit(): void {
-  }
+   }
+getProfileRepos(){}
+  ngOnInit(): void{
+    this.githubservice.getProfileRepos().subscribe((repos:any[])=>{
+      console.log(repos);
+      this.repos= repos;
+    });
 
+}
 }
