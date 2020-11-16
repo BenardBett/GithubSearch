9611aac04550a37b0787b0d691991344ac388ea6
BenardBett <kiplangatbett5@gmail.com>
2020-11-16 16:08:58 +0300

    debug app routing model

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path:'user', component:ProfileComponent},
-  {path:'repositories', component:RepoComponent}
+  {path: 'user', component: ProfileComponent},
+  {path: 'repositories', component: RepoComponent}
 
 ];
 
